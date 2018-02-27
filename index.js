a555c9deeb2fc71dee4f4109d21f2f5da21cef61
ssan9 <perrsy@gmail.com>
2018-02-26 22:33:27 -0500

    added config.js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var EVENTFUL_SEARCH_URL = 'https://api.eventful.com/json/events/search';
+var app_key = config.app_key;
 function getDataFromApi(searchTermOne, searchTermTwo, searchTermThree, dates, callback) {
   var query = {
     location: searchTermOne,
@@ -9,7 +10,7 @@ function getDataFromApi(searchTermOne, searchTermTwo, searchTermThree, dates, ca
     within: searchTermThree,
     units: 'mi',
     sort_order: 'date',
-    app_key: 'FJjthswGhP26qMXR'
+    app_key: config.app_key
   };
 
   $.ajax({
