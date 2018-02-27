4890b28528f8b0b2e5e11da7951c4c2706ae427a
ssan9 <perrsy@gmail.com>
2018-02-26 22:02:50 -0500

    removed config file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getDataFromApi(searchTermOne, searchTermTwo, searchTermThree, dates, ca
     within: searchTermThree,
     units: 'mi',
     sort_order: 'date',
-    app_key: config.app_key
+    app_key: 'FJjthswGhP26qMXR'
   };
 
   $.ajax({
